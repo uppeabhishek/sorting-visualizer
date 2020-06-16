a12cdb5ff07bb6ad4072adc9591e3da10adfeb99
Abhishek <abhishek.uppe@ibigroup.com>
2020-06-16 19:02:42 +0530

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useEffect, useState } from "react";
+import { Header } from "./Header";
 
 function App(): ReactElement {
-    return <div>Hello</div>;
+    const [isLoading, toggleLoading] = useState(true);
+
+    useEffect(() => {
+        const html = document.querySelector("html");
+
+        if (html) {
+            let value = "";
+            const theme = localStorage.getItem("theme");
+
+            if (theme === null) {
+                value = "dark";
+                localStorage.setItem("theme", "dark");
+            } else {
+                value = theme;
+            }
+            html.setAttribute("theme", value);
+            toggleLoading(false);
+        }
+    }, []);
+
+    return !isLoading ? <Header /> : <div />;
 }
 
 export default App;
